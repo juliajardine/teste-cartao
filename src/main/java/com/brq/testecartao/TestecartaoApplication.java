be97d49cee1515a3c6960cebdb9331f77db84328
Julia Jardine <julias.jardin4@gmail.com>
2019-06-04 11:45:15 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/brq/testecartao/TestecartaoApplication.java
@@ -0,0 +1,13 @@
+package com.brq.testecartao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestecartaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestecartaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brq/testecartao/TestecartaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brq.testecartao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestecartaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
